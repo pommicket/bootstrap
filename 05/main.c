95e7ec1ab5c1de11719be901a8b8451a49fa6c7b
pommicket <pommicket@gmail.com>
2022-02-15 16:48:18 -0500

    locale.h

--- 05/main.c
+++ 05/main.c
@@ -4,13 +4,12 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <ctype.h>
-
-typedef struct {
-	int x;
-	long y;
-} S;
+#include <locale.h>
 
 int main(int argc, char **argv) {
+	setlocale(LC_ALL, "C");
+	struct lconv *c = localeconv();
+	printf("%s\n",c->negative_sign);
 	return 0;
 }
 
