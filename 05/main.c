0f97a589b800bdb71dda05984192f0f66a52edaa
pommicket <pommicket@gmail.com>
2022-02-19 19:46:02 -0800

    actual hello world program

--- 05/main.c
+++ 05/main.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char **argv) {
-	printf("%p\n", malloc(1024*16));
-	int *list = malloc(1024*4);
-	printf("%p \n",list);
-	list[1023] = 77;
-	list = realloc(list, 1024*64);
-	printf("%p \n",list);
-	printf("%d\n",list[1023]);
-	free(list);
+	printf("Hello, world!\n");
 	return 0;
 }
 
